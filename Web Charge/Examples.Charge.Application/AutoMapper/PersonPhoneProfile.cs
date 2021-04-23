5dc870a9dfa845c64eff67a6343f6ca7308bf97b
paula <>
2021-04-22 21:54:06 -0300

    Mapper e Cors

---
+++ Web Charge/Examples.Charge.Application/AutoMapper/PersonPhoneProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Examples.Charge.Application.Dtos;
+using Examples.Charge.Domain.Aggregates.PersonAggregate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examples.Charge.Application.AutoMapper
+{
+    public class PersonPhoneProfile : Profile
+    {
+        public PersonPhoneProfile()
+        {
+            CreateMap<PersonPhone, PersonPhoneDTO>()
+               .ReverseMap()
+               .ForMember(dest => dest.BusinessEntityID, opt => opt.MapFrom(src => src.Id))
+               .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
+               .ForMember(dest => dest.PhoneNumberTypeID, opt => opt.MapFrom(src => src.PhoneNumberTypeId));
+
+            CreateMap<PersonPhoneDTO, PersonPhone>()
+               .ReverseMap()
+               .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.BusinessEntityID))
+               .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
+               .ForMember(dest => dest.PhoneNumberTypeId, opt => opt.MapFrom(src => src.PhoneNumberTypeID));
+        }
+    }
+}
