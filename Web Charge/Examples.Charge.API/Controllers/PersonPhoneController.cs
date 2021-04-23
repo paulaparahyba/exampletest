4570858301ca5f1c6b5144c217512889110dc294
paula <>
2021-04-22 21:53:43 -0300

    Primeiro commit

---
+++ Web Charge/Examples.Charge.API/Controllers/PersonPhoneController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Examples.Charge.Application.Dtos;
+using Examples.Charge.Application.Interfaces;
+using Examples.Charge.Application.Messages.Request;
+using Examples.Charge.Application.Messages.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Examples.Charge.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonPhoneController : BaseController
+    {
+        private IPersonPhoneFacade _facade;
+
+        public PersonPhoneController(IPersonPhoneFacade facade, IMapper mapper)
+        {
+            _facade = facade;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PersonPhoneResponse>> Get() => Response(await _facade.FindAllAsync());
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return Response(_facade.FindById(id));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] PersonPhoneRequest request)
+        {
+            var result = _facade.Edit(request.Dto);
+            return Response(0, result);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Response(0, _facade.Delete(id));
+        }
+    }
+}
